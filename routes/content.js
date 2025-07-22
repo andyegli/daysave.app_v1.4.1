b67f50fd89a47e53ca0e14e52abc41c0dfa66956
andyegli <andy.egli@itnetworld.co.nz>
2025-07-22 21:45:46 +0200

    Fix bulk URL analysis: Add missing Facebook share/r/ pattern to isMultimediaURL
    - Added /facebook\.com\/share\/r\//i pattern to isMultimediaURL function
    - This pattern was missing, causing bulk submitted Facebook URLs to be detected as 'video' but not trigger analysis
    - Manually triggered analysis for the 2 missed Facebook URLs from recent bulk upload
    - Both URLs now have full AI analysis: summaries, titles, tags, and transcription
    - Future Facebook share/r/ URLs will now automatically trigger multimedia analysis

--- routes/content.js
+++ routes/content.js
@@ -33,6 +33,7 @@ function isMultimediaURL(url) {
     /facebook\.com\/watch/i,
     /facebook\.com\/share\/v\//i,
     /facebook\.com\/share\/p\//i,
+    /facebook\.com\/share\/r\//i,  // Facebook share/r/ URLs (reels/mixed content)
     /facebook\.com\/video\//i,
     /facebook\.com\/.*\/videos\//i,
     /facebook\.com\/.*\/posts\//i,
