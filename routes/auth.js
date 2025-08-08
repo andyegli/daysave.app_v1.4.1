7eacbcd2c17975f600fdc8e45aad3f277d2afb92
andyegli <andy.egli@itnetworld.co.nz>
2025-08-09 05:31:02 +1200

    🔧 Fix passkey recovery - add missing UserPasskey import in auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 const { logAuthEvent, logAuthError, logOAuthFlow, logOAuthError } = require('../config/logger');
-const { User, Role, Sequelize, SocialAccount, SubscriptionPlan } = require('../models');
+const { User, Role, Sequelize, SocialAccount, SubscriptionPlan, UserPasskey } = require('../models');
 const subscriptionService = require('../services/subscriptionService');
 
 // Import middleware
